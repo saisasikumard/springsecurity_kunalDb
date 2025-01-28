1a177867afde8af119c073546ee7f7cbd06c9489
sai <dhavileswarapusaikumar@gmail.com>
2025-01-29 00:52:32 +0530

    jwt implementation

--- src/main/java/com/obito/acciojob/SpringSecuitiryDBKunal/config/SecurityConfig.java
+++ src/main/java/com/obito/acciojob/SpringSecuitiryDBKunal/config/SecurityConfig.java
@@ -1,20 +1,28 @@
 package com.obito.acciojob.SpringSecuitiryDBKunal.config;
 
+import com.obito.acciojob.SpringSecuitiryDBKunal.filter.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    JwtAuthenticationFilter jwtAuthenticationFilter;
     @Bean
     PasswordEncoder getPasswordEncode(){
         return new BCryptPasswordEncoder();
@@ -29,20 +37,22 @@ public class SecurityConfig {
         httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/public") // Add this specific matcher
+                        .requestMatchers("person/public") // Add this specific matcher
                         .permitAll()
                         .requestMatchers("/person/add") // Add this specific matcher
+                        .permitAll()
+                        .requestMatchers("/person/authenticate") // Add this specific matcher
                         .permitAll()
                         .requestMatchers("/person/find")
                         .hasRole("USER")
 
-                        .anyRequest().authenticated()
-                )
-                .formLogin(form -> form
-                        .loginProcessingUrl("/login")
-                        .permitAll()
-                )
-                .httpBasic(Customizer.withDefaults());
+                        .anyRequest().authenticated())
+                        .sessionManagement(session -> session
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        ).authenticationProvider(getAuthenticatorProvider())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                ;
+
         return httpSecurity.build();
     }
     @Bean
@@ -51,5 +61,9 @@ public class SecurityConfig {
         daoAuthenticationProvider.setUserDetailsService(getUserDetailsService());
         daoAuthenticationProvider.setPasswordEncoder(getPasswordEncode());
         return daoAuthenticationProvider;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration congif) throws Exception {
+        return congif.getAuthenticationManager();
     }
 }
--- src/main/java/com/obito/acciojob/SpringSecuitiryDBKunal/controller/PersonController.java
+++ src/main/java/com/obito/acciojob/SpringSecuitiryDBKunal/controller/PersonController.java
@@ -1,10 +1,15 @@
 package com.obito.acciojob.SpringSecuitiryDBKunal.controller;
 
+import com.obito.acciojob.SpringSecuitiryDBKunal.dto.AuthRequest;
 import com.obito.acciojob.SpringSecuitiryDBKunal.entity.Person;
+import com.obito.acciojob.SpringSecuitiryDBKunal.service.JwtService;
 import com.obito.acciojob.SpringSecuitiryDBKunal.service.PersonService;
 import jakarta.persistence.GeneratedValue;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/person")
 public class PersonController {
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtService jwtService;
     private static final Logger logger = LoggerFactory.getLogger(PersonController.class);
     @Autowired
     private PersonService personService;
@@ -32,5 +41,18 @@ public class PersonController {
     public String welcome(){
         logger.info("Entered in welcome method");
         return "Welcome to public space..";
+    }
+
+    //Api to create jwt token
+    @PostMapping("/authenticate")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest){
+      Authentication authentication =authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUserName(),authRequest.getPassword()));
+       logger.info("entered authenticated method...");
+       if(authentication.isAuthenticated()) {
+           return jwtService.generateToken(authRequest.getUserName());
+       }
+       else{
+           throw new RuntimeException("Invalid UserRequest");
+       }
     }
 }
