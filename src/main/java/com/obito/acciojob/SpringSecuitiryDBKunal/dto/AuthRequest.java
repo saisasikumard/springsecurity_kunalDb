0d1bd49abf20df5401134e0c91ddf7f280ab81e7
sai <dhavileswarapusaikumar@gmail.com>
2025-05-02 09:56:07 +0530

    Added method Level Security by PreAuthorize

--- src/main/java/com/obito/acciojob/SpringSecuitiryDBKunal/dto/AuthRequest.java
+++ src/main/java/com/obito/acciojob/SpringSecuitiryDBKunal/dto/AuthRequest.java
@@ -8,6 +8,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthRequest {
+    private String userName;
+    private String password;
+
     public String getUserName() {
         return userName;
     }
@@ -24,6 +27,5 @@ public class AuthRequest {
         this.password = password;
     }
 
-    private String userName;
-    private String password;
+
 }
